19d0d2375c918c8d09033bcdcc2761fbd5fff38b
Andrew Brown <alpha121bravo@gmail.com>
2018-09-25 08:03:10 +0100

    attempted extensions

--- controller.rb
+++ controller.rb
@@ -4,8 +4,14 @@ require('pry-byebug')
 
 require_relative('./models/game_logic')
 
+get '/' do
+  erb(:home)
+end
+get '/result' do
+  erb(:result)
+end
 get '/compare/:player_1/:player_2' do
   game = GameLogic.new(params[:player_1], params[:player_2])
   @result = game.compare
-  return @result
+  erb(:result)
 end
